2a49c4c6417118a4e63c1b999636a442b371a004
Sean Collings <seanrcollings@gmail.com>
2019-12-26 22:05:09 -0700

    db uses absolute paths now

--- app/api.py
+++ app/api.py
@@ -3,23 +3,28 @@ from app.models import Stat
 from app import db
 api = Blueprint('api', __name__)
 
+
 @api.route('/')
 def root():
     return render_template('landing.html')
-    
+
+
 @api.route('/docs')
 def docs():
     return render_template('docs.html')
+
 
 @api.route('/stats')
 def stats():
     stats = Stat.query.all()
     return jsonify([i.serialize for i in stats])
 
+
 @api.route('/stats/<int:_id>')
 def show(_id):
     stat = Stat.query.get(_id)
     return jsonify(stat.serialize)
+
 
 @api.route('/stats/<int:_id>/data')
 def data(_id):
